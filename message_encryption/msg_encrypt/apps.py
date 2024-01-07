2a910faec29c63d5f1cb07bc5c1f3a10d8b06f53
Jatin Kevlani <119515986+JatinKevlani@users.noreply.github.com>
2024-01-07 15:55:05 +0530

    Delete message_encryption directory

--- message_encryption/msg_encrypt/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MsgEncryptConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'msg_encrypt'
--- message_encryption/msg_encrypt/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from msg_encrypt import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('encrypt', views.encrypt, name='encrypt'),
-    path('decrypt', views.decrypt, name='decrypt'),
-]
