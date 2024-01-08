aa14ea17a96ecbc67812292c15e560cbda4430e3
JatinKevlani <jatinkevlani01@gmail.com>
2024-01-08 17:54:38 +0530

    Fixed some bugs

--- message_encryption/settings.py
+++ message_encryption/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lm83$bm+@dda=2*d5c%5f3a0d0fgxa@=8c+6(uqh#a044q7k=6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
