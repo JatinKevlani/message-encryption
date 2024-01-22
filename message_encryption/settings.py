fcc0e77ffeb995de22ca4718b6d600c60dc5efe4
JatinKevlani <jatinkevlani01@gmail.com>
2024-01-22 15:55:09 +0530

    Vercel settings applied

--- message_encryption/settings.py
+++ message_encryption/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lm83$bm+@dda=2*d5c%5f3a0d0fgxa@=8c+6(uqh#a044q7k=6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -118,9 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
